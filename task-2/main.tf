5d0a28828ce5510ced6fa43a08a6b89423b392d2
charmisanghvi <103896021+charmisanghvi@users.noreply.github.com>
2022-07-25 00:32:42 -0500

    Add files via upload

---
+++ task-2/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "tomcat_server" {
+  ami             = "ami-0cff7528ff583bf9a"
+  instance_type   = "t2.micro"
+  key_name        = "devopskp"
+  security_groups = ["default"]
+  user_data = file("tomcat.sh")
+
+  tags = {
+    Name = "Tomcatserver-terraform"
+  }
+}
+  resource "aws_security_group" "default" {
+	tags = {
+	type = "terraform-test-security-group"
+}
+}
+
+
+
